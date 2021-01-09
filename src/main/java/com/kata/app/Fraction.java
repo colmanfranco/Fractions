103382fd5377c0998fc50643bd7ae20e6552c2b8
Franco Colman <colmanfm@gmail.com>
2021-01-09 17:02:55 +0100

    First commit

---
+++ src/main/java/com/kata/app/Fraction.java
@@ -0,0 +1,13 @@
+package com.kata.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Fraction
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
