3b9db8b85c59e9c2ef8adc995c4fcf40c8373911
Franco Colman <colmanfm@gmail.com>
2021-02-05 11:08:54 +0100

    Small Refactor in tests

--- src/test/java/com/kata/app/AddFractionTestShould.java
+++ src/test/java/com/kata/app/AddFractionTestShould.java
@@ -18,43 +18,36 @@ public class AddFractionTestShould
 
     @Test
     public void sumIntegerPlusZero() {
-        Fraction sum = new Fraction(1).add(new Fraction(0));
-        assertEquals(new Fraction(1), sum);
+        assertEquals(new Fraction(1), new Fraction(1).add(new Fraction(0)));
     }
 
     @Test
     public void sumIntegerPlusInteger() {
-        Fraction sum = new Fraction(1).add(new Fraction(2));
-        assertEquals(new Fraction(3), sum);
+        assertEquals(new Fraction(3), new Fraction(1).add(new Fraction(2)));
     }
 
     @Test
     public void sumNegativePlusPositiveAndObtainPositiveResult() {
-        Fraction sum = new Fraction(-1).add(new Fraction(5));
-        assertEquals(new Fraction(4), sum);
+        assertEquals(new Fraction(4), new Fraction(-1).add(new Fraction(5)));
     }
 
     @Test
     public void sumNegativePlusPositiveAndObtainNegativeResult() {
-        Fraction sum = new Fraction(-5).add(new Fraction(2));
-        assertEquals(new Fraction(-3), sum);
+        assertEquals(new Fraction(-3), new Fraction(-5).add(new Fraction(2)));
     }
 
     @Test
     public void sumPositivePlusNegativeAndObtainPositiveResult() {
-        Fraction sum = new Fraction(7).add(new Fraction(-6));
-        assertEquals(new Fraction(1), sum);
+        assertEquals(new Fraction(1), new Fraction(7).add(new Fraction(-6)));
     }
 
     @Test
     public void sumPositivePlusNegativeAndObtainNegativeResult() {
-        Fraction sum = new Fraction(8).add(new Fraction(-10));
-        assertEquals(new Fraction(-2), sum);
+        assertEquals(new Fraction(-2), new Fraction(8).add(new Fraction(-10)));
     }
 
     @Test
     public void sumNonTrivialEqualsDenominators() {
-        Fraction sum = new Fraction(1, 3).add(new Fraction(1, 3));
-        assertEquals(new Fraction(2, 3), sum);
+        assertEquals(new Fraction(2, 3), new Fraction(1, 3).add(new Fraction(1, 3)));
     }
 }
